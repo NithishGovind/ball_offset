e90377565a4c2fa7cd2887534d3e92410d8216bd
NithishGovind <nithishg82@gmail.com>
2025-07-23 12:21:55 +0530

    final

--- basic_impl.py
+++ basic_impl.py
@@ -1,27 +1,29 @@
 import numpy as np
-
-# Point positions (relative to center)
-x = 1  # e.g., 10 cm
+##center
+x = 1  
 y = 1
+A=100
+B=110
+C=114
+D=104
 
 points = np.array([
-    [ x,  0, 100],  # A
-    [ 0,  y, 110],  # B
-    [-x,  0, 114],  # C
-    [ 0, -y, 104]   # D
+    [ x,  0, A], 
+    [ 0,  y, B], 
+    [-x,  0, C], 
+    [ 0, -y, D]   
 ])
 
-# Fit plane Z = p*x + q*y + r
+
 X = np.c_[points[:,0], points[:,1], np.ones(4)]
 Z = points[:,2]
 
 coeffs, _, _, _ = np.linalg.lstsq(X, Z, rcond=None)
 p, q, r = coeffs
 
-# Plane equation: Z = p*x + q*y + r
+print(coeffs)
 print(f"Plane: Z = {p:.2f}*x + {q:.2f}*y + {r:.2f}")
 
-# Compute how much to raise/lower each screw to reach Z=r (flat)
 adjustments = []
 for pt in points:
     x_i, y_i, z_measured = pt
